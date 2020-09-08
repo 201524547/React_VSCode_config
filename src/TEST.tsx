ed5d90b5ef426d03aa06a42923905ef48d8bbf63
201524547 <qht6@naver.com>
2020-09-08 17:27:18 +0900

    VS Code 프리티어, 린트 환경설정

---
+++ src/TEST.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TEST = () => {
+  return (
+    <div>
+      <div>
+        <form>
+          <div> 아하 !</div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TEST;
